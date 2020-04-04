bd3bda032893a37c0f133890f1a6920f970cea06
gionoa <self@gionoa.dev>
2020-04-04 02:19:33 -0700

    Add Xcode project and .gitignore

---
+++ Porsche Motorsport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Porsche Motorsport
+//
+//  Created by Giovanni Noa on 4/4/20.
+//  Copyright © 2020 Giovanni Noa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
