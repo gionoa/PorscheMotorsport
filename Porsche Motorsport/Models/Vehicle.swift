c2fa804b226747723d3eb47cfc71709d6db51940
gionoa <self@gionoa.dev>
2020-04-05 01:43:19 -0700

    add vehicle attributes to model

---
+++ Porsche Motorsport/Models/Vehicle.swift
@@ -0,0 +1,20 @@
+//
+//  Vehicle.swift
+//  Porsche Motorsport
+//
+//  Created by Giovanni Noa on 4/5/20.
+//  Copyright © 2020 Giovanni Noa. All rights reserved.
+//
+
+import Foundation
+
+struct Vehicle: Identifiable {
+    let id = UUID()
+    let url: String
+    let name: String
+    let power: String
+    let engine: String
+    let weight: String
+    let transmission: String
+    let displacement: String
+}
