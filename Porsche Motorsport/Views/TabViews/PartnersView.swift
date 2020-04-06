615f3dff4ee316d2ab7e7f3e5aa8e1327ac0b2f3
gionoa <self@gionoa.dev>
2020-04-06 00:57:42 -0700

    implement partners view (WIP)

--- Porsche Motorsport/Views/TabViews/PartnersView.swift
+++ Porsche Motorsport/Views/TabViews/PartnersView.swift
@@ -8,10 +8,26 @@
 
 import Foundation
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct PartnersView: View {
     
+    let partners = ["https://files.porsche.com/filestore/image/multimedia/none/rd-2014-motorsport-raceseries-fiawec-teaser-michelin/normal/ca4cbf3b-17df-11e8-bbc5-0019999cd470;s3/porsche-normal.jpg",
+                    "https://files.porsche.com/filestore/image/multimedia/none/rd-2014-motorsport-raceseries-fiawec-teaser-michelin/normal/ca4cbf3b-17df-11e8-bbc5-0019999cd470;s3/porsche-normal.jpg",
+                    "https://files.porsche.com/filestore/image/multimedia/none/rd-2014-motorsport-raceseries-fiawec-teaser-michelin/normal/ca4cbf3b-17df-11e8-bbc5-0019999cd470;s3/porsche-normal.jpg",
+                    "https://files.porsche.com/filestore/image/multimedia/none/rd-2014-motorsport-raceseries-fiawec-teaser-michelin/normal/ca4cbf3b-17df-11e8-bbc5-0019999cd470;s3/porsche-normal.jpg",
+                    "https://files.porsche.com/filestore/image/multimedia/none/rd-2014-motorsport-raceseries-fiawec-teaser-michelin/normal/ca4cbf3b-17df-11e8-bbc5-0019999cd470;s3/porsche-normal.jpg"]
+    
     var body: some View {
-        Text("Yo")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach((0..<partners.count)) { indexPath in
+                    WebImage(url: URL(string: self.partners[indexPath])!)
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }
+        .navigationBarTitle("Porsche Partners")
     }
 }
