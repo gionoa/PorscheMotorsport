d80bb39d9d346d2d98d2803ff68956b6bff80e5d
gionoa <self@gionoa.dev>
2020-04-06 00:56:25 -0700

    add video player UIKit class

--- Porsche Motorsport/ImageStore.swift
+++ Porsche Motorsport/ImageStore.swift
@@ -8,4 +8,5 @@
 
 import Foundation
 
+#warning("Refactor image store")
 let imageURL = "https://files.porsche.com/filestore/galleryimagerwd/multimedia/none/motorsport-racingcars-992-rsr-gallery01/zoom2/c32c3054-5323-11ea-80c8-005056bbdc38;sL/porsche-motorsport-image.jpg"
